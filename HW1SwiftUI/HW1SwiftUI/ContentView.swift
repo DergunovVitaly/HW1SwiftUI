02304e930f33a30a456f5611769896609e0d9cea
Vitalii Dergunov <dergunov.vitaly@gmail.com>
2021-02-28 13:42:21 +0200

    Added tab bar items

--- HW1SwiftUI/HW1SwiftUI/ContentView.swift
+++ HW1SwiftUI/HW1SwiftUI/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            DashboardView()
+                .tabItem {
+                    Text("Main")
+                }
+            DashboardView()
+                .tabItem {
+                    Text("Main")
+                }
+            DashboardView()
+                .tabItem {
+                    Text("Main")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+// MARK: - Views
+
+struct DashboardView: View {
+    var body: some View {
+        Text("Start Page")
     }
 }
