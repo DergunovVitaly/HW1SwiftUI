a16183ef5a9824d21f1c783ec1ae32bd9f990953
Vitalii Dergunov <dergunov.vitaly@gmail.com>
2021-02-28 12:32:58 +0200

    initial commit

---
+++ HW1SwiftUI/HW1SwiftUI/HW1SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW1SwiftUIApp.swift
+//  HW1SwiftUI
+//
+//  Created by Vitalii Dergunov on 28.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW1SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
